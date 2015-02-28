08622835f8d0281d963f5c6f3fdb484694bef4f6
SVonsarovsky <sergey.vonsarovsky@gmail.com>
2015-02-28 18:47:01 +0200

    Adding projects to Travis CI

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,7 +2,11 @@ FactoryGirl.define do
   factory :order do
     user { FactoryGirl.create(:user) }
     factory :order_not_in_progress do
-      state { Array.new(Order::STATES).delete_if{|state| state == Order::STATE_IN_PROGRESS}.sample }
+      state do
+        a = Array.new(Order::STATES)
+        a.delete(Order::STATE_IN_PROGRESS)
+        a.sample
+      end
       completed_at { Time.zone.now }
     end
   end
